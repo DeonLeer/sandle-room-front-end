5451f1cfde7db8dc7e9011ed84ddbdb57a0317bc
DeonLeer <deon.leer96@gmail.com>
2022-10-05 00:20:32 -0700

    fixed routes, testing getadminboard

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useContext } from "react";
 import { Navigate, useRoutes } from "react-router-dom";
 import useToken from "./components/App/useToken";
@@ -11,23 +12,32 @@ import RequestAppointment from "./pages/RequestAppointment";
 import DashboardLayout from "./layouts/Dashboard";
 import Schedule from "./pages/Schedule";
 import { UserContext } from "./App";
+import userService from "./services/user.service";
 
 export default function Router() {
 
+    const [admin, setAdmin] = useState(false)
+    
     const userContext = useContext(UserContext);
+
+    const userServices = userService();
+    
+    useEffect(() => {
+        async function getIsAdmin() {
+            const data = await userServices.getAdminBoard();
+            data.data === "Admin Content." && setAdmin(true)
+        }
+        getIsAdmin()
+    }, [])
 
     const routes = [
         {
             path: "/",
             element: <Home />,
-        },
-        {
-            path: "*",
-            element: <Navigate to="/" />,
-        },
+        }
     ];
 
-    if (userContext.user && userContext.user.roles && userContext.user.roles.includes('ROLE_ADMIN')) {
+    if (userContext.user && userContext.user.roles && userContext.user.roles.includes(2)) {
         routes.push({
             path: "/dashboard",
             element: <DashboardLayout />,
@@ -37,9 +47,10 @@ export default function Router() {
                 { path: "appointments", element: <Appointments /> },
                 { path: "requestappointment", element: <RequestAppointment /> },
                 { path: "preferences", element: <Preferences /> },
+                { path: "*", element: <Navigate to='/'/> }
             ],
         })
-    } else if (userContext.user && userContext.user.roles && userContext.user.roles.includes('ROLE_USER')) {
+    } else if (userContext.user && userContext.user.roles && userContext.user.roles.includes(1)) {
         routes.push({
             path: "/dashboard",
             element: <DashboardLayout />,
@@ -49,13 +60,17 @@ export default function Router() {
                 { path: "appointments", element: <Appointments /> },
                 { path: "requestappointment", element: <RequestAppointment /> },
                 { path: "preferences", element: <Preferences /> },
+                { path: "*", element: <Navigate to='/'/> }
             ],
         })
     } else {
         routes.push({
             path: "/login",
-            element: <Login />}
-        )
+            element: <Login />
+        }, {
+            path: "*",
+            element: <Navigate to='/'/>
+        })
     }
 
 
